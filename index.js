c3d675014b4f9fbc4cf3de1a82ff137afe8b3674
Kyle Wiltshire <kwiltshire@foursquare.com>
2020-02-15 15:27:09 -0500

    Add monthly breakdown

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const axios = require("axios")
 
 let history = 30
 
+let months = {
+  "1": "Jan",
+  "2": "Feb",
+  "3": "Mar",
+  "4": "Apr",
+  "5": "May",
+  "6": "Jun",
+  "7": "Jul",
+  "8": "Aug",
+  "9": "Sep",
+  "10": "Oct",
+  "11": "Nov",
+  "12": "Dec"
+}
+
 let myClients = [
   { "consumerId": "FKXEEBAKS22ILUWEGFK2TI33RW1S45MLUMCQZ1IRA3QMTUQA", "name": "Itemize" },
   { "consumerId": "OBVXKGT02QS1IUOJFXPZCLJE5PKRENVIX1103PGSJMXDVPQK", "name": "Hands Producao E Veiculacao de Media Ltda" },
@@ -26,74 +41,93 @@ let myClients = [
   { "consumerId": "O4HRWENKPWQ2Q0GTVEG1P5ULRY2PO5XALRJHHNJIK5UNPGV0", "name": "Deloitte - The Wombat App" }
 ]
 
-function fetchHitsForMultipleIds(consumers) {
+function fetchHitsForConsumers(consumers) {
   consumers.forEach((consumer) => {
-    fetchDailyHits(history, consumer)
+    fetchHits(history, consumer)
   })
 }
 
-function fetchDailyHits(days, consumer) {
+function fetchHits(days, consumer) {
   let consumerId = consumer.consumerId
   let consumerName = consumer.name
 
   let urlToFetch = `https://api.foursquare.com/v2/apps/dailystats/?v=20191202&days=${days}&consumerIds=${consumerId}&byEndpoint=true&oauth_token=DOFQSWVZGSAB5O52HSBZSBCOOTQPDOGBEEANR3DSGF5PEPV2`
 
-  try {
-    axios.get(urlToFetch).then((response) => {
-      let data = response.data
-      let stats = data.response.stats
+  axios.get(urlToFetch).then((response) => {
+    let data = response.data
+    let stats = data.response.stats
 
-      if (stats === undefined) {
-        console.log({ consumerName, "consumerId": consumerId, "endpoints": "No stats for user" })
-      }
+    if (stats == undefined) {
+      console.log({ consumerName, "consumerId": consumerId, "endpoints": "No stats for user" })
+    }
 
-      let dailyStats = stats[0].dailyStats
+    let dailyStats = stats[0].dailyStats
 
-      filterDailyHits(consumer, dailyStats)
-    })
+    if (dailyStats == undefined) {
+      console.log({ consumerName, "consumerId": consumerId, "endpoints": "No stats for user" })
+    }
 
-  } catch (error) {
-
-    console.log(error)
-  }
+    filterDailyHits(consumer, dailyStats)
+  }).catch((error) => {
+    console.log(error.response)
+  })
 }
 
 function filterDailyHits(consumer, data) {
-  const filteredStats = []
+  const newStats = []
 
   if (data.length != 0) {
     data.filter((stat) => {
       if (stat.totalCount > 0) {
-        filteredStats.push(stat)
+        const newStat = {}
+        const month = months[stat.date.split("-")[1].slice(1)]
+        const year = stat.date.split("-")[0]
+        newStat.month = `${month}-${year}`
+        newStat.endpoints = stat.endpointStats
+        newStats.push(newStat)
       }
     })
-    // reduce endpoints
-    const totalEndpoints = filteredStats.reduce((total, stat) => {
-      stat.endpointStats.forEach((endpoint) => {
-        total.push(endpoint)
-      })
-      return total
-    }, [])
 
-    const uniqueEndpoints = totalEndpoints.reduce((total, endpoint) => {
-      let path = endpoint.path
-      if (!total[path]) {
-        total[path] = endpoint.count
+    //reduce months
+    const endpointsByMonth = newStats.reduce((total, stat) => {
+      let month = stat.month
+      let endpoints = stat.endpoints
+
+      if (!total[month]) {
+        total[month] = endpoints
       } else {
-        total[path] += endpoint.count
+        endpoints.forEach((endpoint) => {
+          total[month].push(endpoint)
+        })
       }
       return total
     }, {})
 
-    console.log({
-      "name": consumer.name, "consumerId": consumer.consumerId, "endpoints": uniqueEndpoints
+    const uniqueEndpointsByMonth = Object.keys(endpointsByMonth).map((month) => {
+      let test = {}
+
+      test.month = month
+      test.paths = endpointsByMonth[month].reduce((total, endpoint) => {
+        let path = endpoint.path
+        let count = endpoint.count
+        if (!total[path]) {
+          total[path] = count
+        } else {
+          total[path] += count
+        }
+
+        return total
+      }, {})
+
+      return test
     })
 
-  } else {
+    console.log({ "name": consumer.name, "consumerId": consumer.consumerId, "usage": uniqueEndpointsByMonth })
 
+  } else {
     console.log({ "name": consumer.name, "consumerId": consumer.consumerId, "endpoints": `No activity` })
   }
 }
 
-fetchHitsForMultipleIds(myClients)
-// fetchDailyHits(30, "5FVCWV5PEP2FPNAIBHO3CT4K04LWBJI5GH2F3PJ5LJQVXUEK")
+fetchHitsForConsumers(myClients)
+/* fetchHits(30, "BCTFBU31G2OJGGPU5W5MSAJGPFDH2NREK3CBAC0PXJVN052G") */
